cf5e966da24c46a6a3dca6ece389a1a8c722f4d9
MasterShredder <immortalshredder@gmail.com>
2023-10-06 00:49:09 +0400

    Улучшения ИИ
    Фиксы ИИ против дэшей черепах: убран лишний паттерн, добавлена новая проверка и улучшена проверка из предыдущего коммита.

--- bank_1A.asm
+++ bank_1A.asm
@@ -6766,19 +6766,17 @@ C - - - - - 0x036775 0D:A765: 90 31     BCC bra_A798
 - - - - - - 0x036785 0D:A775: AD 38 06  LDA ram_0638
 - - - - - - 0x036788 0D:A778: C9 58     CMP #$58
 - - - - - - 0x03678A 0D:A77A: B0 19     BCS bra_A795
+- - - - - - 0x036796 0D:A786: BD 20 05  LDA ram_obj_0520,X
+                                        CMP #con_plr_state_брошен_соперником
+                                        BEQ bra_A78F
+                                        CMP #con_plr_state_делает_суперку
+- - - - - - 0x03679B 0D:A78B: F0 02     BNE bra_A78D
                                         LDA ram_plr_062C,X
                                         CMP #$11
                                         BCS bra_A78F
-
+bra_A78D:
                                         LDA ram_0638
 - - - - - - 0x03678C 0D:A77C: D9 FE BF  CMP tbl_BFFE,Y
-- - - - - - 0x03678F 0D:A77F: BD C0 04  LDA ram_obj_04C0,X
-- - - - - - 0x036792 0D:A782: 49 09     EOR #$09
-- - - - - - 0x036794 0D:A784: D0 07     BNE bra_A78D
-- - - - - - 0x036796 0D:A786: BD 20 05  LDA ram_obj_0520,X
-- - - - - - 0x036799 0D:A789: 49 08     EOR #con_plr_state_делает_суперку
-- - - - - - 0x03679B 0D:A78B: F0 02     BEQ bra_A78F
-bra_A78D:
 - - - - - - 0x03679D 0D:A78D: B0 09     BCS bra_A798
 bra_A78F:
 - - - - - - 0x03679F 0D:A78F: A9 04     LDA #con_шаблон_ai_04
